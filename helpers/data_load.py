24f4c81cc9f9ed7167100a26fd3a44dd1aed34f0
imranskiy11 <imranskiy11@gmail.com>
2022-12-25 20:25:56 +0300

    return to dev fin

---
+++ helpers/data_load.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from typing import List
+
+def load_data_from_txt(path: str, sep: str='\t', skipinitialspace: bool=True, header=None, 
+    names:List[str] =['EURUSD', 'num' 'date', 'time', 'open', 'max', 'min', 'close', 'vol']):
+    """
+    Load data from txt file
+    """
+    return pd.read_csv(
+        path, sep=sep, skipinitialspace=skipinitialspace, 
+        header=header, names=names)
+
+
+if __name__ == '__main__':
+    print()
---
+++ main.py
@@ -0,0 +1,8 @@
+from helpers.data_load import load_data_from_txt
+
+
+
+
+if __name__ == '__main__':
+    data = load_data_from_txt(path='data/data.txt')
+    
