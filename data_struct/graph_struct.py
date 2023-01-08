edfcc0d0a2708d931e944a90e2e67ae9b045bd38
imranskiy11 <imranskiy11@gmail.com>
2023-01-08 16:09:06 +0300

    struct changes

---
+++ data_struct/graph_struct.py
@@ -0,0 +1,9 @@
+class GraphSequence:
+    
+    def __init__(self, sequence) -> None:
+        
+        self.sequence = sequence
+        self.start_value = sequence[0]
+        
+            
+    
--- data_struct/head_tail_struct.py
+++ data_struct/head_tail_struct.py
@@ -3,7 +3,7 @@ from matplotlib.pyplot import plot, show
 
 class HeadTail:
     
-    def __init__(self, sequence, head_size=100, tail_size=25):
+    def __init__(self, sequence, head_size=100, tail_size=25, change_value=0.00001):
         
         self.head_size = head_size
         self.tail_size = tail_size
@@ -12,6 +12,20 @@ class HeadTail:
         
         self.head = sequence[:self.head_size]
         self.tail = sequence[-self.tail_size:]
+        
+        self.change_value = change_value
+    
+    @property
+    def tail_conclusion(self):
+        if abs(self.tail[-1] - self.tail[0]) < self.change_value:
+            return None
+        if self.tail[-1] - self.tail[0] >= self.change_value:
+            return 1
+        if self.tail[-1] - self.tail[0] <= (-1)*self.change_value:
+            return 0
+
+    def set_change_value(self, change_value):
+        self.change_value = change_value
    
     def show_plot(
         self, head=True, tail=True, window_show=True, head_color='g', tail_color='r',):
