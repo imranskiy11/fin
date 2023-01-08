6e3e23cce83e9f770993fa8822ec1384eb0bc631
imranskiy11 <imranskiy11@gmail.com>
2023-01-08 14:12:13 +0300

    rm ipynb | add headtailstruct

---
+++ data_struct/head_tail_struct.py
@@ -0,0 +1,45 @@
+import numpy as np
+from matplotlib.pyplot import plot, show
+
+class HeadTail:
+    
+    def __init__(self, sequence, head_size=100, tail_size=25):
+        
+        self.head_size = head_size
+        self.tail_size = tail_size
+        
+        self.sequence = sequence
+        
+        self._build_struct(sequence)
+        
+        
+        
+        
+    def _build_struct(self, sequence):
+        self.head = sequence[:self.head_size]
+        self.tail = sequence[-self.tail_size:]
+        
+        
+    def show_plot(
+        self, head=True, tail=True, window_show=True, head_color='g', tail_color='r',):
+        # """
+        # # colors:
+        # # ‘b’ blue,   ‘g’ green, ‘r’ red
+        # # ‘c’ cyan,   ‘m’ magenta
+        # # ‘y’ yellow, ‘k’ black, ‘w’ white
+        
+        # """
+        print('sequence plot')
+        if head:
+            plot(np.arange(0, self.head_size), 
+                 self.head, 
+                 color=head_color)
+        if tail:
+            plot(
+                np.arange(self.head_size - 1, self.head_size + self.tail_size), 
+                self.sequence[self.head_size - 1 : self.head_size + self.tail_size],
+                color=tail_color)
+
+        if window_show:
+            show()
+# if __name__ == '__main__':
